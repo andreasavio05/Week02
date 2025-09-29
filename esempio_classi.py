d11bdac2f878b14cb7ffa96108259d42cac8ecfd
fabriziolamberti <41021126+fabriziolamberti@users.noreply.github.com>
2025-09-29 15:18:40 +0200

    Material developed on Monday, September 29, 2025

---
+++ esempio_classi.py
@@ -0,0 +1,39 @@
+# Demonstrating the use of classes, e.g., defining class Book
+
+# The class describes the structure of a book, with its attributes
+
+class Book:
+    def __init__(self, t, a, n): # Parameters to initialize the book instance (object)
+        self.title = t # These variables store values of attributes for a real book
+        self.author = a
+        self.num_pages = n
+
+# There is no real book yet (no instance), only their structure (abstraction)
+
+# Starting from the Book class, it is possible to create a Book instance (reification),
+# passing as arguments to function init() the values each attribute has to assume
+
+book1 = Book("Il nome della rosa", "U. Eco", 800)
+
+# Printing a Book variable as done below the memory reference is printed, not object content
+
+print(book1)
+
+# To print object content it is necessary to access object's attributes with the dot notation .
+
+print(book1.title, book1.author, book1.num_pages)
+
+# With the code that follows, another (different) instance (book) is created, with different information
+
+book2 = Book("I pilastri della terra", "K. Follet", 1000)
+
+print(book2.title, book2.author, book2.num_pages)
+
+# Like other data, books can be stored in data containers, like lists
+
+books = [book1, book2]
+
+# And the data container can be iterated as seen already
+
+for book in books:
+    print(book.title, book.author, book.num_pages)
